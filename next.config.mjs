29200c3c53c4a7d842a9cf88f4bb561ae43d8046
Ananya <ananyar0912@gmail.com>
2026-01-02 15:58:25 +0530

    configuring next.js

--- next.config.mjs
+++ next.config.mjs
@@ -1,12 +1,4 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  experimental: {
-    appDir: true,
-  },
-  output: 'standalone',
-  reactStrictMode: true,
-};
+const nextConfig = {};
 
 export default nextConfig;
-
-
